1510ba2ee02bc63ffe89b023b274a7220dd52890
Kyle <dahaydnator@gmail.com>
2024-04-22 17:38:55 -0500

    Added renderLicenseLink + renderLicenseBadge functions; Added generateMarkdown function; finished index.js; added readME

---
+++ Develop/README.md
@@ -0,0 +1,35 @@
+# README Generator
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+  App for generating README files
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+  N/A
+
+  ## Usage
+  N/A
+
+  ## License
+    This README generator is licensed under the [MIT License](https://opensource.org/licenses/MIT)
+  
+
+  ## Badges
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Contribute
+  N/A
+
+  ## Tests
+  N/A
+
+  ## Questions
+  Feel free to reach out and contact me with any questions you may have.
+  - GitHub: [KyleH-Git](https://github.com/KyleH-Git)
+  - Email: dahaydnator@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("README generated.");
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("Leave entries blank if N/A.");
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Enter README title:',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Enter a project description:',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Enter installation instructions:',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Enter usage instructions:',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Describe how to contribute to the project:',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'Describe project tests:',
+            name: 'tests',
+        },
+        {
+            type: 'list',
+            message: 'Please select a license:',
+            name: 'license',
+            choices: ['No License', 'MIT License', 'GNU GPL v3'],
+        },
+        {
+            type: 'input',
+            message: 'Please enter your GitHub username:',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'Please enter your Email:',
+            name: 'email',
+        }
+
+    ])
+    .then((data) => {
+        writeToFile("README.md", generateMarkdown(data));
+    });
+  
+}
 
 // Function call to initialize app
 init();
